867ae367665f851b9c4bd83bd4d4fbee9d236f15
Heath, James <jamesheath@my.unt.edu>
2024-01-29 14:23:09 -0600

    graphing added

--- collatz.js
+++ collatz.js
@@ -1,3 +1,7 @@
+import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
+
+
+
 function primeFactorsList(n) {
     let factors = []
     let factor = 2
@@ -48,7 +52,7 @@ const tag = (tag, content) => `<${tag}>${content}</${tag}>`
 
 window.onload = () => {
     let ex1 = document.getElementById("ex1")
-    ex1.innerHTML = range(1,100,1).map((x) => tag("li", sequence(x).join(" -> "))).join("")
+    ex1.innerHTML = range(1,100,1).map((x) => tag("li", `S(${x}) : ${sequence(x).join(", ")}`)).join("")
 
     let ex2 = document.getElementById("ex2")
     ex2.innerHTML = range(1,100,2).map((x) => tag("li", odd_sequence(x).join(" -> "))).join("")
@@ -58,5 +62,44 @@ window.onload = () => {
 
     let ex4 = document.getElementById("ex4")
     ex4.innerHTML = range(1,100,2).map((x) => tag("li", x.toString() + " : " + odd_sequence(x).map((y) => y.toString(2)).join(" -> "))).join("")
+
+    let collatzLength = range(1,100).map((n) => ({"n" : n, "Collatz Sequence Length" : sequence(n).length, "Type" : "red"}))
+    let plot = Plot.auto(collatzLength, {"x" : "n", "y" : "Collatz Sequence Length"}).plot()
+    let div = document.getElementById("g1");
+    div.append(plot);
+
+    let collatzOddLength = range(1,100).map((n) => ({"n" : n, "Collatz Odd Sequence Length" : odd_sequence(n).length, "Type" : "blue"}))
+    plot = Plot.auto(collatzOddLength, {"x" : "n", "y" : "Collatz Odd Sequence Length"}).plot()
+    div = document.getElementById("g2");
+    div.append(plot);
+
+
+    plot = Plot.plot({
+        marks : [
+            Plot.ruleY([0]),
+            Plot.lineY(collatzLength, {x: "n", y: "Collatz Sequence Length", stroke: "red"}),
+            Plot.lineY(collatzOddLength, {x: "n", y: "Collatz Odd Sequence Length", stroke: "blue"})
+        ]
+    })
+    div = document.getElementById("g3");
+    div.append(plot);
+    
+    let oddRatio = range(1,300).map((n) => ({"n" : n, "ratio": sequence(n).length / odd_sequence(n).length}))
+    plot = Plot.auto(oddRatio, {"x" : "n", "y" : "ratio"}).plot() 
+    div = document.getElementById("g4");
+    div.append(plot);
+
+    let binaryFractal = range(2,63,2).map((x) => {
+        let count = 0
+        let y = x
+        while(y%2 === 0) {
+            count++
+            y /= 2
+        } 
+        return {"n" : x, "Factors of 2" : count}
+    })
+    plot = Plot.barY(binaryFractal, {"x" : "n", "y" : "Factors of 2"}).plot() 
+    div = document.getElementById("g5");
+    div.append(plot);
 }
 
